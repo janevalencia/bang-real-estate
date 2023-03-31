b417786ad48ee1a05cf1ed1df1d00bc3dba5f406
Jane Valencia <31334333+janevalencia@users.noreply.github.com>
2023-03-31 22:28:06 +0800

    Remove cypress dependency

--- cypress/tests/e2e/example/example.cy.js
+++
@@ -1,9 +0,0 @@
-describe('Navigation', () => {
-    it('should render homepage', () => {
-        // Start from the index page
-        cy.visit('http://localhost:3000/');
-
-        // Should display the hello greeting.
-        cy.get('[data-cy=welcome-greeting]').contains(/hello/i);
-    })
-})
